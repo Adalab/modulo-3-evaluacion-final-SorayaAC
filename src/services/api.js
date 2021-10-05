a9a3197c662005b7ae70afb114847cba2f95daaa
Soraya Alonso <salonsocuesta@gmail.com>
2021-10-05 13:28:16 +0200

    ok last commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,20 +1,19 @@
-
 function api() {
-    return fetch(`https://raw.githubusercontent.com/Adalab/rick-y-morty/master/data/rick-y-morty.json`)
-    .then(response => response.json())
-    .then(json => {
-        return json.results.map(character => {
-            return{
-            id: character.id,
-            photo: character.image, 
-            name: character.name,
-            specie: character.species,
-            planet:character.location.name,
-            episodes:character.episode,
-            lifestatus:character.status
-            }
-        })
-    })
+  return fetch(`https://rickandmortyapi.com/api/character`)
+    .then((response) => response.json())
+    .then((json) => {
+      return json.results.map((character) => {
+        return {
+          id: character.id,
+          photo: character.image,
+          name: character.name,
+          specie: character.species,
+          planet: character.location.name,
+          episodes: character.episode,
+          lifestatus: character.status,
+        };
+      });
+    });
 }
 
 export default api;
